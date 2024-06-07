1bf6c96f9a23b8a07cf575f3ee6e97dc85624e8a
NhialR10 <nhiallualcho@gmail.com>
2024-06-07 21:59:17 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignUp from "./Pages/LoginSignUp";
+import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/men"
+            element={<ShopCategory banner={men_banner} Category="men" />}
+          />
+          <Route
+            path="/women"
+            element={<ShopCategory banner={women_banner} Category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} Category="kid" />}
+          />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/Login" element={<LoginSignUp />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/ShopCategory.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import "./Css/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import Item from "../Components/Item/Item";
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className="shop-category">
+      <img className="ShopCategory-banner" src={props.banner} alt="" />
+      <div className="shopCategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> Out of 36 Products
+        </p>
+
+        <div className="shopCategory-Sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+
+      <div className="shopCategory-Products">
+        {all_product.map((item, i) => {
+          if (props.Category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ShopCategory;
